dcb0f7bf0a6f2d1dec1839c04b3daece83fc39eb
Dan Higham <dan.higham@gmail.com>
2020-03-15 14:20:38 -0700

    Update to run on PWS

--- config.js
+++ config.js
@@ -6,6 +6,10 @@
 };
 */
 
+var cfenv = require("cfenv")
+var appEnv = cfenv.getAppEnv()
+
+
 var argv = require('yargs')
         .usage('Usage: $0 [--port INTEGER [8080]] [--baseurl STRING ["/"]] [--redis STRING:INT [127.0.0.1:6379]] [--gaEnabled] [--gaAccount STRING [UA-2069672-4]]')
         .argv;
@@ -20,9 +24,16 @@ exports.googleanalytics = {
 	account: argv['gaAccount'] || "UA-2069672-4"
 };
 
+var vcap_services = process.env.VCAP_SERVICES;
+var rediscloud_service = JSON.parse(vcap_services)["rediscloud"][0]
+var credentials = rediscloud_service.credentials;
+
+var redisURL = 'redis://:' + credentials.password + '@' + credentials.hostname + ':' + credentials.port
+
+console.log(redisURL)
 exports.database = {
 	type: 'redis',
 	prefix: '#scrumblr#',
-	redis: argv.redis || '127.0.0.1:6379'
+	redis: argv.redis || redisURL || 'redis://127.0.0.1:6379'
 };
 
